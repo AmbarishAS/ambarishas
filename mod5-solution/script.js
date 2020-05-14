663abb03afff421ffb93f024da56401d278ce982
AmbarishAS <65124600+AmbarishAS@users.noreply.github.com>
2020-05-14 18:37:54 +0530

    Update script.js

--- mod5-solution/script.js
+++ mod5-solution/script.js
@@ -13,10 +13,10 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
 var dc = {};
 
-var homeHtmlUrl = "snippets/home-snippet.html";
+var homeHtmlUrl = "home-snippet.html";
 var allCategoriesUrl =
   "https://davids-restaurant.herokuapp.com/categories.json";
-var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+var categoriesTitleHtml = "categories-title-snippet.html";
 var categoryHtml = "category-snippet.html";
 var menuItemsUrl =
   "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
