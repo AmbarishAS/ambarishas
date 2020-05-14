63b44b73827b5cd11814aad3dbcc03616d8ac74e
AmbarishAS <65124600+AmbarishAS@users.noreply.github.com>
2020-05-14 18:36:14 +0530

    Update script.js

--- mod5-solution/script.js
+++ mod5-solution/script.js
@@ -17,11 +17,11 @@ var homeHtmlUrl = "snippets/home-snippet.html";
 var allCategoriesUrl =
   "https://davids-restaurant.herokuapp.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-var categoryHtml = "snippets/category-snippet.html";
+var categoryHtml = "category-snippet.html";
 var menuItemsUrl =
   "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
-var menuItemsTitleHtml = "snippets/menu-items-title.html";
-var menuItemHtml = "snippets/menu-item.html";
+var menuItemsTitleHtml = "menu-items-title.html";
+var menuItemHtml = "menu-item.html";
 
 // Convenience function for inserting innerHTML for 'select'
 var insertHtml = function (selector, html) {
